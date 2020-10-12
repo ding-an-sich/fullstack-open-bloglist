13f1097ebb6176f2f1dd2b009c4ee7b433b93c74
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-10-12 13:58:04 -0300

    Refactored blogs controller

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 const app = express()
 
@@ -17,24 +17,7 @@ mongoose.connect(mongoUrl, {
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then((blogs) => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const { PORT } = process.env
 app.listen(PORT, () => {
