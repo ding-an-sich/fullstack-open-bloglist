596d15dffb605cc9f03ffca35a2d28b589d9e20f
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-10-12 13:30:08 -0300

    Initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2020: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'airbnb'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    semi: 0,
+    'comma-dangle': 0,
+    'no-console': 0
+  },
+};
