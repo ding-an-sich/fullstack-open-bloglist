872a34cdeaa44576c74a6617da6a344b5dedc544
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-10-16 18:22:20 -0300

    Fix blog tests

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -11,16 +11,20 @@ beforeEach(async () => {
   await User.deleteMany({})
   await Blog.deleteMany({})
 
+  const rootUser = {
+    username: 'root',
+    name: 'SuperMan',
+    password: 'admin123'
+  }
+
+  await api
+    .post('/api/users')
+    .send(rootUser)
+
   const blogObjects = initialBlogs.map((blog) => new Blog(blog))
   const promises = blogObjects.map((blogObject) => blogObject.save())
   await Promise.all(promises)
 })
-
-const populateWithUsers = async (users) => {
-  const userObjects = users.map((user) => new User(user))
-  const promises = userObjects.map((userObject) => userObject.save())
-  await Promise.all(promises)
-}
 
 /* Alternative version with for..of loop
  * Slower but executes each save in order
@@ -35,6 +39,13 @@ beforeEach(async () => {
   }
 })
 */
+
+// These users won't get hashed passwords
+const populateWithUsers = async (users) => {
+  const userObjects = users.map((user) => new User(user))
+  const promises = userObjects.map((userObject) => userObject.save())
+  await Promise.all(promises)
+}
 
 describe('when listing blogs', () => {
   test('blogs are returned as JSON', async () => {
@@ -58,6 +69,16 @@ describe('when listing blogs', () => {
 describe('when adding a blog', () => {
   test('a blog can be added', async () => {
     await populateWithUsers(mockUsers)
+
+    const loginInfo = {
+      username: 'root',
+      password: 'admin123'
+    }
+
+    let response = await api
+      .post('/api/login')
+      .send(loginInfo)
+
     const newBlog = {
       title: 'I have nothing interesting to say',
       author: 'vmrc',
@@ -67,16 +88,26 @@ describe('when adding a blog', () => {
 
     await api
       .post('/api/blogs')
+      .set('Authorization', `bearer ${response.body.token}`)
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
 
-    const response = await api.get('/api/blogs')
+    response = await api.get('/api/blogs')
     expect(response.body).toHaveLength(initialBlogs.length + 1)
   })
 
   test('a blog added without likes will default to 0', async () => {
     await populateWithUsers(mockUsers)
+
+    const loginInfo = {
+      username: 'root',
+      password: 'admin123'
+    }
+
+    let response = await api
+      .post('/api/login')
+      .send(loginInfo)
 
     const newBlog = {
       title: 'I have nothing interesting to say',
@@ -86,13 +117,23 @@ describe('when adding a blog', () => {
 
     await api
       .post('/api/blogs')
+      .set('Authorization', `bearer ${response.body.token}`)
       .send(newBlog)
 
-    const response = await api.get('/api/blogs')
+    response = await api.get('/api/blogs')
     expect(response.body[initialBlogs.length].likes).toBe(0)
   })
 
   test('a blog added without title and/or url will not be added', async () => {
+    const loginInfo = {
+      username: 'root',
+      password: 'admin123'
+    }
+
+    let response = await api
+      .post('/api/login')
+      .send(loginInfo)
+
     const newBlog = {
       author: 'vmrc',
       likes: 42
@@ -100,24 +141,51 @@ describe('when adding a blog', () => {
 
     await api
       .post('/api/blogs')
+      .set('Authorization', `bearer ${response.body.token}`)
       .send(newBlog)
       .expect(400)
 
-    const response = await api.get('/api/blogs')
+    response = await api.get('/api/blogs')
     expect(response.body).toHaveLength(initialBlogs.length)
   })
 })
 
 describe('when deleting a blog', () => {
   test('sending a correct id deletes the corresponding blog', async () => {
-    let response = await api.get('/api/blogs')
+    // Creating the blog
+
+    const loginInfo = {
+      username: 'root',
+      password: 'admin123'
+    }
+
+    let response = await api
+      .post('/api/login')
+      .send(loginInfo)
+
+    const { token } = response.body
+
+    const newBlog = {
+      title: 'Will delete',
+      author: 'root',
+      url: 'www.willdelete.com',
+      likes: 100
+    }
+
+    response = await api
+      .post('/api/blogs')
+      .set('Authorization', `bearer ${token}`)
+      .send(newBlog)
+
+    const { id } = response.body
 
     await api
-      .delete(`/api/blogs/${response.body[0].id}`)
+      .delete(`/api/blogs/${id}`)
+      .set('Authorization', `bearer ${token}`)
       .expect(204)
 
     response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(initialBlogs.length - 1)
+    expect(response.body).toHaveLength(initialBlogs.length)
   })
 
   test('sending an incorrect id returns an exception', async () => {
